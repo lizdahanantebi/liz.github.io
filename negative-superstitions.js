3c291cb66f8ae03db0863bb7b510cb055505b8d4
lizdahanantebi <liz.dahan@gmail.com>
2025-05-21 12:07:12 +0300

    Update negative-superstitions.js

--- negative-superstitions.js
+++ negative-superstitions.js
@@ -3,13 +3,13 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 	var API = new APIConstructor();
 	return stiatExtension({
 		category : { 
-			name : 'Negative Superstitions', //Will appear in the data.
+			name : 'Superstitions', //Changed from 'Negative Superstitions' to just 'Superstitions'
 			title : {
-				media : {word : 'Negative Superstitions'}, //Name of the category presented in the task.
+				media : {word : 'Superstitions'}, //Changed category title
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
-			media : [ //Stimuli content as PIP's media objects
+			media : [ //Stimuli content as PIP's media objects - these remain the same
 				{image : 'N_blackcat.png'}, 
 				{image : 'N_brokenmirror.png'}, 
 				{image : 'N_friday.png'}, 
@@ -28,15 +28,15 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
-			media : [ //Stimuli
-				{word: 'Awkward'},
-				{word: 'Hate'},
-				{word: 'Failure'},
-				{word: 'Slum'},
-				{word: 'Stink'}
+			media : [ //Stimuli - replacing words with images
+				{image: 'N_scull.png'},
+				{image: 'N_brokenheart.png'},
+				{image: 'N_unlike.png'},
+				{image: 'N_sad.png'},
+				{image: 'N_fire.png'} // Fire symbol replacing warning
 			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			//Adjusted CSS for images
+			css : {color:'#31b404', 'max-width':'100px', 'max-height':'100px', width:'100px', height:'100px'}
 		},
 		attribute2 : 
 		{
@@ -46,15 +46,15 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
-			media : [ //Stimuli
-				{word: 'Brilliant'},
-				{word: 'Diamond'},
-				{word: 'Joy'},
-				{word: 'Truth'},
-				{word: 'Sunrise'}
+			media : [ //Stimuli - replacing words with images
+				{image: 'P_gift.png'},
+				{image: 'P_heart.png'},
+				{image: 'P_like.png'},
+				{image: 'P_smile.png'},
+				{image: 'P_sun.png'}
 			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			//Adjusted CSS for images
+			css : {color:'#31b404', 'max-width':'100px', 'max-height':'100px', width:'100px', height:'100px'}
 		},
 		base_url : {//Where are your images at?
 			image : 'https://raw.githubusercontent.com/lizdahanantebi/liz.github.io/main/superstition_images/'
