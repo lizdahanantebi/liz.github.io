fe0cfbe4a305142fc7475e2d1254e6b88651822b
lizdahanantebi <liz.dahan@gmail.com>
2025-05-22 14:45:47 +0300

    Update positive-superstitions.js

--- positive-superstitions.js
+++ positive-superstitions.js
@@ -1,71 +1,8 @@
 define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
-	return stiatExtension({
-		category : { 
-			name : 'Superstitions', //Changed from 'Positive Superstitions' to just 'Superstitions'
-			title : {
-				media : {word : 'Superstitions'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli content as PIP's media objects - these remain the same
-				{image : 'P_shootingstar.png'}, 
-				{image : 'P_penny.png'}, 
-				{image : 'P_crossedfingers.png'}, 
-				{image : 'P_clover.png'},
-				{image : 'P_dice.png'},
-				{image : 'P_clothes.png'}
-			], 
-			//Stimulus css (style)
-			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
-		},	
-		attribute1 : 
-		{
-			name : 'Bad', //Attribute label
-			title : {
-				media : {word : 'Bad'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli - replacing words with images
-				{image: 'N_scull.png'},
-				{image: 'N_brokenheart.png'},
-				{image: 'N_unlike.png'},
-				{image: 'N_sad.png'},
-				{image: 'N_fire.png'} // Fire symbol replacing warning
-			], 
-			//EXACTLY the same CSS as for the category images
-			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
-		},
-		attribute2 : 
-		{
-			name : 'Good', //Attribute label
-			title : {
-				media : {word : 'Good'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli - replacing words with images
-				{image: 'P_gift.png'},
-				{image: 'P_heart.png'},
-				{image: 'P_like.png'},
-				{image: 'P_smile.png'},
-				{image: 'P_sun.png'}
-			], 
-			//EXACTLY the same CSS as for the category images
-			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
-		},
-		base_url : {//Where are your images at?
-			image : 'https://raw.githubusercontent.com/lizdahanantebi/liz.github.io/main/superstition_images/'
-		}
-	});
-	base_url : {//Where are your images at?
-			image : 'https://raw.githubusercontent.com/lizdahanantebi/liz.github.io/main/superstition_images/'
-		}
-	});
-
-	// Add logger for Qualtrics - הוסף את זה כאן
+	
+	// Add logger for Qualtrics
 	API.addSettings('logger', {
 		onRow: function(logName, log, settings, ctx){
 			if (!ctx.logs) ctx.logs = [];
@@ -89,7 +26,61 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 			window.minnoJS.onEnd();
 		}
 	});
-
-	return API.script;
-});
+	
+	return stiatExtension({
+		category : { 
+			name : 'Superstitions',
+			title : {
+				media : {word : 'Superstitions'},
+				css : {color:'#31b404','font-size':'2em'},
+				height : 7
+			}, 
+			media : [
+				{image : 'P_shootingstar.png'}, 
+				{image : 'P_penny.png'}, 
+				{image : 'P_crossedfingers.png'}, 
+				{image : 'P_clover.png'},
+				{image : 'P_dice.png'},
+				{image : 'P_clothes.png'}
+			], 
+			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
+		},	
+		attribute1 : 
+		{
+			name : 'Bad',
+			title : {
+				media : {word : 'Bad'},
+				css : {color:'#31b404','font-size':'2em'},
+				height : 7
+			}, 
+			media : [
+				{image: 'N_scull.png'},
+				{image: 'N_brokenheart.png'},
+				{image: 'N_unlike.png'},
+				{image: 'N_sad.png'},
+				{image: 'N_fire.png'}
+			], 
+			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
+		},
+		attribute2 : 
+		{
+			name : 'Good',
+			title : {
+				media : {word : 'Good'},
+				css : {color:'#31b404','font-size':'2em'},
+				height : 7
+			}, 
+			media : [
+				{image: 'P_gift.png'},
+				{image: 'P_heart.png'},
+				{image: 'P_like.png'},
+				{image: 'P_smile.png'},
+				{image: 'P_sun.png'}
+			], 
+			css : {color:'#31b404','font-size':'3em', 'max-width':'200px', 'max-height':'200px', width:'200px', height:'200px', border:'3px solid black'}
+		},
+		base_url : {
+			image : 'https://raw.githubusercontent.com/lizdahanantebi/liz.github.io/main/superstition_images/'
+		}
+	});
 });
