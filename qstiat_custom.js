44fad6169852121decd405228c99023d3b2e100f
lizdahanantebi <liz.dahan@gmail.com>
2025-05-30 21:08:24 +0300

    Update qstiat_custom.js

--- qstiat_custom.js
+++ qstiat_custom.js
@@ -140,8 +140,10 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			orText : 'or', 
 			orCss : {'font-size':'1.8em', color:'#000000'},
 
-			//הסרתי את ההודעה שמופיעה כל הזמן - תופיע רק בהסברים
-			remindErrorText : '',
+			//Will appear at the bottom of the screen during trials.
+			remindErrorText : '<p align="center" style="font-size:"0.6em"; font-family:arial">' +
+			'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+			'Press the other key to continue.<p/>',
 			
 			finalText: 'You have completed this task<br/><br/>Press SPACE to continue.', 
 
@@ -155,7 +157,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<font color="#000000"><u>Part blockNum of nBlocks</u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
 				'Put a left finger on the <b>E</b> key for items that belong to the category ' + 
-				'<font color="#31b404">attribute1</font>.<br/>' + 
+				'<font color="#cc0000">attribute1</font>.<br/>' + 
 				'Put a right finger on the <b>I</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute2</font>.<br/>' + 
 				'Items will appear one at a time.<br/><br/>' + 
@@ -166,7 +168,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
 				'Put a left finger on the <b>E</b> key for items that belong to the category ' + 
-				'<font color="#31b404">attribute1</font>.<br/>' + 
+				'<font color="#cc0000">attribute1</font>.<br/>' + 
 				'Put a right finger on the <b>I</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute2</font> ' +
 				'and for items that belong to the category <font color="#007acc">thecategory</font>.<br/>' + 
@@ -178,7 +180,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
 				'Put a left finger on the <b>E</b> key for items that belong to the category ' + 
-				'<font color="#31b404">attribute1</font> ' +
+				'<font color="#cc0000">attribute1</font> ' +
 				'and for items that belong to the category <font color="#007acc">thecategory</font>.<br/>' + 
 				'Put a right finger on the <b>I</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute2</font>.<br/>' + 
@@ -461,13 +463,13 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				css:piCurrent.category.css,
 				media : {inherit:{type:'exRandom',set:'category'}}
 			}],			
-			// this stimulus used for giving feedback, in this case only the error notification - ה-X למטה
+			// this stimulus used for giving feedback, in this case only the error notification
 			error : [{
-				data:{handle:'error'}, location: {top: 85}, css:{color:'red','font-size':'4em'}, media: {word:'X'}, nolog:true
+				data:{handle:'error'}, location: {top: 70}, css:{color:'red','font-size':'4em'}, media: {word:'X'}, nolog:true
 			}],
-			// הוספת גירוי timeout - ההודעה למעלה בשחור
+			// הוספת גירוי timeout
 			timeoutStim : [{
-				data:{handle:'timeoutStim'}, location: {top: 70}, css:{color:'black','font-size':'2em'}, media: {word:'Please respond faster'}, nolog:true
+				data:{handle:'timeoutStim'}, location: {top: 70}, css:{color:'red','font-size':'2em'}, media: {word:'Please response faster'}, nolog:true
 			}],
 			dummyForLog : [{
 				data:{name:'dummyForLog', alias:'dummyForLog'}, 
