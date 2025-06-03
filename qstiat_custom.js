ccad002f678694db88710e62f1cf68b4b2cd691a
lizdahanantebi <liz.dahan@gmail.com>
2025-06-03 22:25:54 +0300

    Update qstiat_custom.js

--- qstiat_custom.js
+++ qstiat_custom.js
@@ -171,7 +171,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<font color="#31b404">attribute1</font>.<br/>' + 
 				'Put a right finger on the <b>I</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute2</font> ' +
-				'and for items that belong to the category <font color="#000000"><b>thecategory</b></font>.<br/>' + 
+				'and for items that belong to the category <font color="#0066cc">thecategory</font>.<br/>' + 
 				'Items will appear one at a time.<br/><br/>' + 
 				'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' + 
 				'Press the other key to continue.<br/><br/>' + 
@@ -181,7 +181,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
 				'Put a left finger on the <b>E</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute1</font> ' +
-				'and for items that belong to the category <font color="#000000"><b>thecategory</b></font>.<br/>' + 
+				'and for items that belong to the category <font color="#0066cc">thecategory</font>.<br/>' + 
 				'Put a right finger on the <b>I</b> key for items that belong to the category ' + 
 				'<font color="#31b404">attribute2</font>.<br/>' + 
 				'Items will appear one at a time.<br/><br/>' + 
