e11525f5d7df1cc21cfbc67a3662d0d927df4a57
lizdahanantebi <liz.dahan@gmail.com>
2025-06-05 19:31:54 +0300

    Update positive-superstitions.js

--- positive-superstitions.js
+++ positive-superstitions.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://lizdahanantebi.github.io/liz.github.io/qstiat_custom.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://lizdahanantebi.github.io/liz.github.io/qstiat_custom.js?v=positive'], function(APIConstructor, stiatExtension){
 
     var API = new APIConstructor();
 
